7d45e6a095c12bf6a17902c833c52f439f35d3c9
Andrew Stein <ABStein@github.com>
2019-12-12 10:27:37 -0600

    added another test case for facebook

--- spec/features/facebook_api_spec.rb
+++ spec/features/facebook_api_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 describe 'Testing a Facebook API' do
   it 'should hit the api and get back full name with id' do
     facebook_api_key = ENV["FACEBOOK_ACCESS_TOKEN"]
-
     get "https://graph.facebook.com/v5.0/me?fields=id%2Cfirst_name%2Clast_name&access_token=#{facebook_api_key}"
 
     expect_json(
@@ -18,7 +17,6 @@ describe 'Testing a Facebook API' do
 
   it 'should hit the api with improper creds' do
     bad_facebook_api_key = ENV["BAD_FACEBOOK_ACCESS_TOKEN"]
-
     get "https://graph.facebook.com/v5.0/me?fields=id%2Cfirst_name%2Clast_name&access_token=#{bad_facebook_api_key}"
 
     expect_json(
@@ -34,7 +32,6 @@ describe 'Testing a Facebook API' do
 
   it 'should hit the api with an improper field name' do
     facebook_api_key = ENV["FACEBOOK_ACCESS_TOKEN"]
-
     get "https://graph.facebook.com/v5.0/me?fields=id%2Cfirst_na%2Clast_name&access_token=#{facebook_api_key}"
 
     expect_json(
@@ -47,4 +44,28 @@ describe 'Testing a Facebook API' do
 
     expect_status(400)
   end
+
+  it 'should hit the api with the access_token field misspelled' do
+    facebook_api_key = ENV["FACEBOOK_ACCESS_TOKEN"]
+    get "https://graph.facebook.com/v5.0/me?fields=id%2Cfirst_na%2Clast_name&accs_token=#{facebook_api_key}"
+
+    expect_json(
+      "error": {
+         "message": "An active access token must be used to query information about the current user.",
+         "type": "OAuthException",
+         "code": 2500,
+      }
+    )
+
+    expect_status(400)
+  end
+
+  it 'should create a post for account given' do
+    facebook_api_key = ENV["FACEBOOK_ACCESS_TOKEN"]
+
+    post "https://graph.facebook.com/10162770320265438/feed
+    ?message=Awesome!
+    &access_token=#{facebook_api_key}"
+  end
+
 end
