201ee65453efc998fd0a20a709341985e22672f0
Andrew Stein <ABStein@github.com>
2019-12-12 10:02:28 -0600

    added more test cases for the facebook api

--- spec/features/dark_sky_api_spec.rb
+++ spec/features/dark_sky_api_spec.rb
@@ -72,4 +72,36 @@ describe 'Testing a Facebook API' do
 
     expect_status(200)
   end
+
+  it 'should hit the api with improper creds' do
+    bad_facebook_api_key = ENV["BAD_FACEBOOK_ACCESS_TOKEN"]
+
+    get "https://graph.facebook.com/v5.0/me?fields=id%2Cfirst_name%2Clast_name&access_token=#{bad_facebook_api_key}"
+
+    expect_json(
+         error: {
+            message: "The access token could not be decrypted",
+            type: "OAuthException",
+            code: 190
+         }
+    )
+
+    expect_status(400)
+  end
+
+  it 'should hit the api with an improper field name' do
+    facebook_api_key = ENV["FACEBOOK_ACCESS_TOKEN"]
+
+    get "https://graph.facebook.com/v5.0/me?fields=id%2Cfirst_na%2Clast_name&access_token=#{facebook_api_key}"
+
+    expect_json(
+      "error": {
+         "message": "(#100) Tried accessing nonexisting field (first_na) on node type (User)",
+         "type": "OAuthException",
+         "code": 100,
+      }
+    )
+
+    expect_status(400)
+  end
 end
